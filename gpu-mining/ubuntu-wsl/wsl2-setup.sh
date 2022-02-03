efcd9fbe62dcd7199e96fd42ce33fc72b1d3d69c
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-02 22:14:50 -0500

    completed wsl setup

--- gpu-mining/ubuntu-wsl/wsl2-setup.sh
+++ gpu-mining/ubuntu-wsl/wsl2-setup.sh
@@ -12,22 +12,16 @@ sudo apt update && sudo apt-get update
 sudo apt dist-upgrade
 sudo apt install -y wget
 
-# Download NVIDIA CUDA toolkit
-sudo apt install nvidia-cuda-toolkit
-nvcc --version
-
-# Configure Driver Software
-??
-
 # Configure CUDA repository
-sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
 
 # Download NVIDIA CUDA
 sudo apt update
-sudo apt install cuda
-sudo apt upgrade nvidia-cuda-toolkit cuda -y
+sudo apt-get install -y cuda-toolkit-11-0
 
 # Download Mining Software: LOL Miner
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
